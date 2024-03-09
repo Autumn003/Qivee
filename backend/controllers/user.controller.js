f0e6f51bd2fdc7799c54495e57eba5fec1df81b9
Hemant <davidbroddy1@gmail.com>
2024-03-09 16:46:40 +0530

    added functionality of update image in profile inside user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -179,7 +179,23 @@ const updateProfile = asyncHandler(async (req, res) => {
     email: req.body.email,
   };
 
-  // we will add cloudinary latter
+  if (req.body.avatar !== "") {
+    const user = await User.findById(req.user.id);
+    const imageId = user.avatar.public_id;
+
+    await cloudinary.uploader.destroy(imageId);
+
+    const myCloud = await cloudinary.uploader.upload(req.body.avatar, {
+      folder: "avatars",
+      width: 150,
+      crop: "scale",
+    });
+
+    newUserData.avatar = {
+      public_id: myCloud.public_id,
+      url: myCloud.url,
+    };
+  }
 
   const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
     new: true,
